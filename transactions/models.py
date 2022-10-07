3d44b8ccba07ab4a546d860c637cf621f0bba625
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-10-07 12:12:01 -0300

    Fix: models  rules fixed

--- transactions/models.py
+++ transactions/models.py
@@ -18,10 +18,10 @@ class TransactionsChoices(models.TextChoices):
 class Transactions(models.Model):
     uuid: models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     type = models.CharField(max_length=30, choices=TransactionsChoices.choices)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField()
     value = models.IntegerField(max_length=10)
     cpf = models.CharField(max_length=11)
     card = models.IntegerField(max_length=12)
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField()
     store_owner = models.CharField(max_length=14)
     store_name = models.CharField(max_length=19)
