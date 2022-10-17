d739158746e94ca67fa76b4027da5db86a54b7cd
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-10-16 23:56:32 -0300

    Fix: loop condition clean code

--- transactions/utils/mixins.py
+++ transactions/utils/mixins.py
@@ -4,28 +4,27 @@ from rest_framework.status import HTTP_201_CREATED
 
 class TraitDataMixin:
     def create(self, request, *args, **kwargs):
-
+        types = [
+            "Debito",
+            "Boleto",
+            "Financiamento",
+            "Credito",
+            "Recebimento Emprestimo",
+            "Vendas",
+            "Recebimento TED",
+            "Recebimento DOC",
+            "Aluguel",
+        ]
         data_parse = []
-
         for line in request.data:
-            if line["type"] == "1":
-                line["type"] = "Debito"
-            elif line["type"] == "2":
-                line["type"] = "Boleto"
-            elif line["type"] == "3":
-                line["type"] = "Financiamento"
-            elif line["type"] == "4":
-                line["type"] = "Credito"
-            elif line["type"] == "5":
-                line["type"] = "Recebimento Emprestimo"
-            elif line["type"] == "6":
-                line["type"] = "Vendas"
-            elif line["type"] == "7":
-                line["type"] = "Recebimento TED"
-            elif line["type"] == "8":
-                line["type"] = "Recebimento DOC"
-            elif line["type"] == "9":
-                line["type"] = "Aluguel"
+            i = 0
+            condition = True
+            while condition:
+                if line["type"] == str(i + 1):
+                    line["type"] = types[i]
+                    condition = False
+                else:
+                    i += 1
             line[
                 "date"
             ] = f"{line['date'][0:4]}-{line['date'][4:6]}-{line['date'][6:8]}"
